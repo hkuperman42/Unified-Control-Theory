ca5153981a4c2c404e73aa8d2f7f092b4a1674e9
hkuperman3 <kup@gatech.edu>
2023-07-19 15:48:55 -0400

    Implement sample and particle weight calculations

--- policies/SteinMPC.py
+++ policies/SteinMPC.py
@@ -1,5 +1,20 @@
 import numpy as np
 
+"""
+Things to consider/check:
+- Is there a faster ordering of axis?
+- Is this usable with a GPU?
+- Are the particle weights the average per particle over the cost-likelihood function?
+- Should I be staying in log-world for longer
+"""
+
+"""
+TODO:
+- Implement update_particles() lmao
+- Add support for different covariances for each particles
+- Enable alternate real-control sampling methods in equation (24)
+"""
+
 
 class SteinMPC:
     def __init__(
@@ -12,23 +27,70 @@ class SteinMPC:
         dynamics,
     ) -> None:
         self.means = means
-        self.cov = cov  # Should probably change to allow for multiple covariances...
-
+        self.cov = cov
         self.cost_likelihood_funct = cost_likelihood_funct
         self.num_particles = num_particles
         self.num_samples_per_particle = num_samples_per_particle
-
         self.dynamics = dynamics
         self.dim_u = dynamics.dim_u
         self.dim_x = dynamics.dim_x
 
-    def setup_run(self, x0, time_horizon):
-        self.state = x0
-        self.time_horizon = time_horizon
+    def set_cost_functions(
+        self,
+        control_cost_funct,
+        step_cost_funct,
+        terminal_cost_funct,
+        cost_likelihood_funct,
+    ):
+        self.control_cost_funct = control_cost_funct
+        self.state_cost_funct = step_cost_funct
+        self.terminal_cost_funct = terminal_cost_funct
+        self.cost_likelihood_funct = cost_likelihood_funct
 
     def sample_particles(self):
-        return np.random.multivariate_normal(
+        return np.random.Generator.multivariate_normal(
             np.zeros(self.dim_u),
             self.cov,
-            size=(self.num_particles, self.time_horizon, self.num_samples_per_particle),
-        ) + np.tile(self.means, (1, 1, self.num_samples_per_particle, self.dim_u))
+            size=(
+                self.num_particles,
+                self.num_samples_per_particle,
+                self.time_horizon,
+            ),
+        ) + np.tile(self.means, self.num_samples_per_particle).transpose((0, 3, 1, 2))
+
+    def simulate_samples(self, samples):
+        states = np.zeros(
+            self.num_particles,
+            self.num_samples_per_particle,
+            self.time_horizon,
+            self.dim_x,
+        )
+        current_states = np.tile(
+            self.state, (self.num_particles, self.num_samples_per_particle, 1)
+        )
+        costs = np.zeros(self.num_particles, self.num_samples_per_particle)
+
+        for i in range(self.time_horizon):
+            sampled_controls = samples[:, :, i, :]
+            current_states = self.dynamics.simulate_timestep(
+                current_states, sampled_controls
+            )
+            states[:, :, i, :] = current_states
+            costs += self.control_cost_funct(sampled_controls) + self.state_cost_funct(
+                current_states
+            )
+
+        costs += self.terminal_cost_funct(current_states)
+
+        return states, costs
+
+    def apply_cost_likelihood(self, costs):
+        cost_weights = self.cost_likelihood_funct(costs)
+        particle_weights = np.sum(cost_weights, axis=1)
+        particle_weights = np.sum(particle_weights)
+
+        return particle_weights, cost_weights
+
+    def update_particles(self):
+        # Do something here
+        return 0
