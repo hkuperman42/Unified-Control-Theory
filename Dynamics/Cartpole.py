0422f99ecf34f20d70f8ff250bfdcda06e565c78
hkuperman3 <kup@gatech.edu>
2023-07-18 20:33:32 -0400

    Add particle sampling, more consistent naming

--- Dynamics/Cartpole.py
+++ Dynamics/Cartpole.py
@@ -1,39 +1,62 @@
 import numpy as np
 import math
 
-class Cartpole():
-    def __init__(self, mQ, mC, l, g) -> None:
-        self.mQ = mQ;
+
+class Cartpole:
+    def __init__(self, mQ, mC, l, g, dim_x, dim_u) -> None:
+        self.mQ = mQ
         self.mC = mC
-        self.l = l;
-        self.g = g;
-        
+        self.l = l
+        self.g = g
+
         self.M = self.mQ + self.mC
         self.ml = self.mQ * self.l
-    
-    
-    def simulateTimestep(self, prevStates : np.ndarray, uSamples : np.ndarray):
-        #For readability (and speed, probably?)
-        x      = prevStates[:, 0]
-        dx     = prevStates[:, 1]
-        theta  = prevStates[:, 2]
-        dtheta = prevStates[:, 3]
 
-        #Get trig functions of our theta-values, since we use them a lot
+        self.dim_x = dim_x
+        self.dim_u = dim_u
+
+    def setCostFunction(self, step_cost_funct, terminal_cost_funct):
+        self.step_cost_funct = step_cost_funct
+        self.terminal_cost_funct = terminal_cost_funct
+
+    def setNoise(self, noise_cov, noise_mean):
+        self.noise_cov = noise_cov
+        self.noise_mean = noise_mean
+
+    def simulateTimestep(self, prev_states: np.ndarray, u_samples: np.ndarray):
+        # For readability (and speed, probably?)
+        x = prev_states[:, 0]
+        dx = prev_states[:, 1]
+        theta = prev_states[:, 2]
+        dtheta = prev_states[:, 3]
+
+        # Get trig functions of our theta-values, since we use them a lot
         sinTheta = np.sin(theta)
         cosTheta = np.cos(theta)
 
-        #Generate second derivatives
-        d2theta = (self.M * self.g * sinTheta) - (self.ml * dtheta * dtheta * sinTheta * cosTheta) - (uSamples * cosTheta)
-        d2theta = d2theta / ((self.M * self.l * 4 / 3) - (self.ml * cosTheta * cosTheta))
-        d2x = uSamples + (self.ml * dtheta * dtheta * sinTheta) - (self.ml * d2theta * cosTheta)
+        # Generate second derivatives
+        d2theta = (
+            (self.M * self.g * sinTheta)
+            - (self.ml * dtheta * dtheta * sinTheta * cosTheta)
+            - (u_samples * cosTheta)
+        )
+        d2theta = d2theta / (
+            (self.M * self.l * 4 / 3) - (self.ml * cosTheta * cosTheta)
+        )
+        d2x = (
+            u_samples
+            + (self.ml * dtheta * dtheta * sinTheta)
+            - (self.ml * d2theta * cosTheta)
+        )
         d2x = d2x / self.M
 
-        #Concatinate back into a single matrix of states
-        nextStates = np.empty_like(prevStates)
+        # Concatinate back into a single matrix of states
+        nextStates = np.empty_like(prev_states)
         nextStates[:, 0] = x + (dx * self.T)
         nextStates[:, 1] = dx + (d2x * self.T)
-        nextStates[:, 2] = np.mod(math.pi + theta + (dtheta * self.T), 2 * math.pi) - math.pi
+        nextStates[:, 2] = (
+            np.mod(math.pi + theta + (dtheta * self.T), 2 * math.pi) - math.pi
+        )
         nextStates[:, 3] = dtheta + (d2theta * self.T)
 
         return nextStates
--- policies/SteinMPC.py
+++ policies/SteinMPC.py
@@ -1,20 +1,34 @@
 import numpy as np
 
-class SteinMPC():
-    def __init__(self, means, sigmas, num_particles, num_samples_per_particle, cost_likelihood_funct) -> None:
-        self.means = means;
-        self.sigmas = sigmas;
-        
-        self.cost_likelihood_funct = cost_likelihood_funct;
-        self.num_particles = num_particles;
-        self.num_samples_per_particle = num_samples_per_particle;
-        
+
+class SteinMPC:
+    def __init__(
+        self,
+        means,
+        cov,
+        num_particles,
+        num_samples_per_particle,
+        cost_likelihood_funct,
+        dynamics,
+    ) -> None:
+        self.means = means
+        self.cov = cov  # Should probably change to allow for multiple covariances...
+
+        self.cost_likelihood_funct = cost_likelihood_funct
+        self.num_particles = num_particles
+        self.num_samples_per_particle = num_samples_per_particle
+
+        self.dynamics = dynamics
+        self.dim_u = dynamics.dim_u
+        self.dim_x = dynamics.dim_x
+
     def setup_run(self, x0, time_horizon):
         self.state = x0
         self.time_horizon = time_horizon
-        
-    
-        
-        
-    
-    
+
+    def sample_particles(self):
+        return np.random.multivariate_normal(
+            np.zeros(self.dim_u),
+            self.cov,
+            size=(self.num_particles, self.time_horizon, self.num_samples_per_particle),
+        ) + np.tile(self.means, (1, 1, self.num_samples_per_particle, self.dim_u))
